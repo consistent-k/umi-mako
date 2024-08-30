3c6820a566b074aa26cd8def9314fabba8f0536b
consistent-k <67736912+consistent-k@users.noreply.github.com>
2024-08-30 09:05:10 +0000

    add less

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
 import Markdown from 'react-markdown'
+import styles from './index.less';
+
 
 const markdown = '# Hi, *Mako*!'
 
 export default function HomePage() {
   return (
-    <div>
-      <Markdown>{markdown}</Markdown>
-    </div>
+    <>
+      <div className={styles.textBox}>
+        Text: 
+        <p>{markdown}</p>
+      </div>
+      <div className={styles.markdownBox}>
+        Markdown: 
+        <Markdown>{markdown}</Markdown>
+      </div>
+    </>
   );
 }
